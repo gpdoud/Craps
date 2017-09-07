5fb8a30eb88feb0e573664157bd7a8de787dfe2c
Greg doud <gpdoud@gmail.com>
2017-09-07 10:28:52 -0400

    Finished basic Craps game

--- Craps/Program.cs
+++ Craps/Program.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Craps {
 	class Program {
+		Random rnd = new Random();
+		int DiceRoll() {
+			var diceone = rnd.Next(6) + 1;
+			var dicetwo = rnd.Next(6) + 1;
+			Print($"Roll is {diceone + dicetwo}");
+			return diceone + dicetwo;
+		}
+		void CrapsGame() {
+			// First Roll
+			var dice = DiceRoll();
+			if(dice == 7 || dice == 11) {
+				Print($"You Won!");
+				return;
+			} else if(dice == 2 || dice == 3 || dice == 12) {
+				Print($"You Lose...");
+				return;
+			}
+			var thePoint = dice;
+			Print($"Point is {thePoint}");
+			// Second thru Nth roll
+			while (true) { // infinite loop
+				dice = DiceRoll();
+				if (dice == thePoint) {
+					Print($"You Won!");
+					return;
+				} else if (dice == 7) {
+					Print($"You Lose...");
+					return;
+				}
+			}
+
+		}
+		void Run() {
+			var TimesToRun = 10;
+			var counter = 0;
+			while(counter < TimesToRun) {
+				CrapsGame();
+				counter++;
+			}
+		}
+		void Print(string msg) {
+			Console.WriteLine(msg);
+		}
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
